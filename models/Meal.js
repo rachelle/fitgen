0a0706369b0f2db974465a925a556f575cd2dd6a
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-24 13:45:44 -0700

    added meal model

---
+++ models/Meal.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose'); 
+var Schema   = mongoose.Schema; 
+
+/* required models */
+var User = require('./User'); 
+var Meal = require('./Meal'); 
+
+/* Define Meal Schema model */
+
+var Meal = new mongoose.Schema({
+    meal_name:      String, 
+    drink_name:     String, 
+    meal_cals:      String, 
+    drink_cals:     String, 
+    meal_protein:   String, 
+    drink_protein:  String,
+    snack_name:     String, 
+    snack_cals:     String, 
+    snack_protein:  String, 
+    user: {
+    type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User'
+    } 
+});
+
+/* exports Schema */
+module.exports = mongoose.model('Meal', Meal);
