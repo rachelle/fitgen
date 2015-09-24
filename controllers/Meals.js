1576c474768b0fbae5d50a27604dc1f2b1932f82
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-24 15:44:07 -0700

    refactored meals controller

--- controllers/Meals.js
+++ controllers/Meals.js
@@ -1,115 +1,110 @@
-var express   = require('express'); 
-var mongoose  = require('mongoose');
+var express = require('express'); 
+var mongoose = require('mongoose'); 
 
 /* source in required models */
 var Meal = require('../models/Meal'); 
 var User = require('../models/User'); 
 
-/* require express router */
-var router = express.Router(); 
+var router = express.Router();
 
-/* get all user meals */
-module.exports.renderMealsIndex = function(req, rex, next) {
-  Meal.find(function(err, meals) {
-    if (error) res.send('> ' + error); 
+/* gets all users meals */
+module.exports.renderMealsIndex = function(req, res, next){
+  Photo.find(function(err, meals){
+    if (err) res.send('> ' + err);
       res.render('./meals', 
       {
         meals: meals, 
-        user:  req.user
+        user: req.user
       });
   });
 };
 
-/* renders a new meal for user */
-module.exports.renderMealsNew = function(req, res) {
-  var meals = Meal.all
+/* renders a new user meal */
+module.exports.renderMealsNew = function(req, res){
+  var meals = Meal.all 
     res.render('./meals/new', {user: req.user, meals:meals});
 };
 
-/* creates a new meal */
-module.exports.renderMealsCreate = function(req, res, next) {
+module.exports.renderMealsCreate = function(req, res, next){
   var meal = new Meal({
-    meal_name: req.body.meal_name, 
-    drink_name: req.body.drink_name, 
-    meal_cals: req.body.meal_cals, 
-    drink_cals: req.body.drink_cals, 
-    meal_protein: req.body.meal_protein, 
-    drink_protein: req.body.drink_protein, 
-    snack_name: req.body.snack_name, 
-    snack_cals: req.body.snack_cals, 
-    snack_protein: req.body.snack_protein,
-    user_id: req.user
-  });
+    meal_name:      req.body.meal_name,
+    meal_protein:   req.body.meal_protien, 
+    meal_cals:      req.body.meal_cals, 
+    drink_name:     req.body.drink_name, 
+    drink_cals:     req.body.drink_cals, 
+    drink_protein:  req.body.drink_protein, 
+    snack_name:     req.body.snack_name, 
+    snack_cals:     req.body.snack_cals, 
+    snack_protein:  req.body.snack_protein, 
+    user_id:       req.user.id
+  });   
   console.log(req.body); 
   meal.save(function(error){
-    if(error){ res.send('> ' + error);}
-      req.user.meals.push(exercise); 
-      req.user.save();
-      res.redirect('./meals/' + meal.id); 
+    if(error){res.send('> ', + error);}
+     
+      res.redirect("/meals/" + meal.id);
   });
 };
 
-/* edit users meal */
-module.exports.renderMealsEdit = function(req, res, next) {
-  var id        = req.params.id; 
-  var meal_id:  = req.params.id; 
+/* edit photo */
+module.exports.renderMealsEdit = function(req, res, next){
+  var id = req.params.id; 
+  var meal_id = req.params.id; 
 
-  Meal.findById({_id:id}, function(error, meal) {
+  Meal.findById({_id:id}, function(error, meal){
     console.log('meal', meal); 
     if(error) res.send(error); 
       res.render(
-        './meals/edit', {
+       './meals/edit', {
           meal: meal, 
           user: req.user
       });
   })
 };
 
-/* updates a meal */
-module.exports.renderMealsUpdate = function(req, res, next) {
+
+module.exports.renderMealsUpdate = function(req, res, next){
   var id = req.params.id; 
 
-  Meal.findById({_id:id}, function(error, meal) {
-    if (error) res.send(error); 
-      if (req.body.meal_name)     meal.meal_name      = req.body.meal_name; 
-      if (req.body.drink_name)    meal.drink_name     = req.body.drink_name;
-      if (req.body.meal_cals)     meal.meal_cals      = req.body.meal_cals;
-      if (req.body.drink_cals)    meal.drink_cals     = req.body.drink_cals;
+  Meal.findById({_id:id}, function(error, meal){
+    if(error) res.send(error); 
+      if (req.body.meal_name)     meal.meal_name      = req.body.meal_name;
+      if (req.body.meal_cals)     meal.meal_cals      = req.body.meal_cals; 
       if (req.body.meal_protein)  meal.meal_protein   = req.body.meal_protein; 
+      if (req.body.drink_name)    meal.drink_name     = req.body.drink_name;
+      if (req.body.drink_cals)    meal.drink_cals     = req.body.drink_cals; 
       if (req.body.drink_protein) meal.drink_protein  = req.body.drink_protein; 
       if (req.body.snack_name)    meal.snack_name     = req.body.snack_name; 
       if (req.body.snack_cals)    meal.snack_cals     = req.body.snack_cals; 
-      if (req.body.snack_protein) meal.snack_protein  = req.body.snack_protein; 
-  
+      if (req.body.snack_protein) meal.snack_protein  = req.body.snack_protein;
+     
       meal.save(function(error) {
         if (error) res.send(error); 
-          res.redirect('/meals/' + id); 
+          res.redirect('/meals/' + id);
       });
   });
 };
 
-/* renders to meal show page */
 module.exports.renderMealsShow = function(req, res, next) {
   var id = req.params.id; 
 
-  Meal.findById(_id: id}, function (error, meal) {
-    if (error) res.send(error); 
+  Meal.findById({_id: id}, function (error, meal){
+    console.log('meal', meal);
+    if(error) res.send(error); 
       res.render(
-        './meals/show', {
-          meal: meal, 
-          user: req.user
-        });
-  });
-};
-
-module.exports.deleteMeal = function(req, res) {
-  var id      = req.params.id; 
-  var meal_id = req.params.id; 
-
-  Meal.findByIdAndRemove({_id:id}, function (error) {
-    if (error) res.send(error); 
-      res.redirect('/meals')
+      './meals/show', {
+        meal: meal, 
+        user: req.user
+      });
   }); 
 };
 
+module.exports.deleteMeal = function(req, res){
+  var id = req.params.id; 
+  var meal_id = req.params.id; 
 
+  Meal.findByIdAndRemove({_id:id}, function (error){
+    if (error) res.send(error);
+      res.redirect('/meals')
+  }); 
+};
