9cb498d930b10ca157931fc5479573daa866a3f4
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-26 12:46:34 -0700

    moved the socketio configuration to express.js in the config folder

--- config/express.js
+++ config/express.js
@@ -1,1 +1,52 @@
-express.js
+var config          = require('./config'); 
+var http            = require('http');
+var socket.io       = require('socket.io'); 
+var express         = require('express'); 
+var morgan          = require('morgan'); 
+var compress        = require('compression'); 
+var bodyParser      = require('body-parser'); 
+var methodOverride  = require('method-override');
+var session         = require('session'); 
+var flash           = require('flash'); 
+var passport        = require('passport');
+
+module.expots = function() {
+  var app = express(); 
+  var server = http.createServer(app); 
+  var io = socketio.listen(server); 
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev')); 
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
+
+  app.use(bodyParser.urlencoded({
+    extended: true, 
+  }));
+
+  app.use(bodyParser.json()); 
+  app.use(methodOverride()); 
+
+  app.use(session({
+    save.uninitialized: true, 
+    resave: true, 
+    secret: config.sessionSecret
+  }));
+
+  app.set('views', './app/views'); 
+  app.set('view engine', 'ejs'); 
+
+  app.use(flash()); 
+  app.use(passport.initialize()); 
+  app.use(passport.session()); 
+
+  require('../app/routes/index.server.routes.js')(app); 
+  require('../app/routes/users.server.routes.js')(app);
+  require('../app/routes/articles.server.routes.js')(app);
+
+  app.use(express.static('./public'));
+
+  return server; 
+
+};
