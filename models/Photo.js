d55734ffc526db78f0947240bb6d4b3aa402bcd9
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-28 17:51:45 -0700

    reference comment model in photo model

--- models/Photo.js
+++ models/Photo.js
@@ -2,14 +2,16 @@ var mongoose = require("mongoose");
 var Schema   = mongoose.Schema; 
 
 /* require user model */
-var User  = require('./User'); 
-var Photo = require('./Photo');
+var User    = require('./User'); 
+var Photo   = require('./Photo');
+var Comment = require('./Comment');
 
 /* Define Photo Schema */
 var Photo = new mongoose.Schema({
     url:      String, 
     image:    String, 
     caption:  String,
+    comments: [Comment.schema],
     user: {
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'User'
