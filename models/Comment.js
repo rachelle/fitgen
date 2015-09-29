dd8089826f1e1ee8d93be30f199da4b49ddc9179
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-28 19:30:27 -0700

    reference users and photos for the comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,25 @@
 var mongoose = require('mongoose'); 
+var Schema   = mongoose.Schema; 
 
+/* required models for comment */
+var User    = require('./User'); 
+var Photo   = require('./Photo'); 
+var Comment = require('./Comment'); 
 
+/* Defining Comment Schema */
 var Comment = mongoose.model('Comment', CommentSchema); 
+    content: String, 
+    created: Date, 
+    user: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'User'
+    }, 
 
-module.exports = Comment;
+    photo: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Photo'
+    }
+}); 
+
+/* exports Schema */
+module.exports = mongoose.model('Comment', Comment);
