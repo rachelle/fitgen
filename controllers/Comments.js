19a6e7f5b1254123d364ab403de2ed932d5b8b0c
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-29 01:37:16 -0700

    creating logic to show comments in photo model

--- controllers/Comments.js
+++ controllers/Comments.js
@@ -9,22 +9,18 @@ var User    = require('../models/User');
 var router = express.Router(); 
 
 
-
-module.exports.renderCommentsIndex = function ( req, res ){
-  Comment.find( function ( err, comments, count ){
-    res.render( 'index', {
-        title : 'Comment System with Mongoose and Node',
-        comments : comments
+module.exports.renderCommentsCreate = function (req, res, next) { 
+  Photo.findOne({_id: request.params.photo_id}, function(error, photo) {
+    if(error) return response.send(error); 
+    photo.comments.push({
+      content: request.body.content, 
+      user:    request.body.user
     });
-  });
-}; 
-
-module.exports.renderCommentsCreate = function ( req, res ){
-  new Comment({
-    username : req.body.username,
-    content : req.body.comment,
-    created : Date.now()
-  }).save( function( err, comment, count ){
-    res.redirect( '/' );
-  });
-};
+    photo.save(function(error) {
+      if(error) return response.send(error); 
+      response.send({
+        success: true
+      });
+    }); 
+  }); 
+});
